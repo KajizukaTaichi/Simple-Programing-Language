a337294ef4769ccbccd8ea3ba51dba64154d2d0e
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-10-18 13:10:16 +0900

    リファクタリング

--- src/executor.rs
+++ src/executor.rs
@@ -2,6 +2,7 @@ use rand::Rng;
 use std::io::{self, Write};
 use std::process::exit;
 
+/// 標準入力を取得する
 fn input(prompt: &str) -> String {
     print!("{}", prompt);
     io::stdout().flush().unwrap();
@@ -30,56 +31,6 @@ pub struct Func {
     code: String,
 }
 
-/// 変数の重複を削除する
-fn remove_duplicates_variable(memory: &mut Vec<Variable>) -> &mut Vec<Variable> {
-    let mut seen_names = std::collections::HashMap::new();
-    let mut to_remove = Vec::new();
-
-    for (index, memory) in memory.iter().enumerate() {
-        if let Some(existing_index) = seen_names.get(&memory.name) {
-            to_remove.push(if existing_index < &index {
-                *existing_index
-            } else {
-                index
-            });
-        } else {
-            seen_names.insert(&memory.name, index);
-        }
-    }
-
-    to_remove.sort(); // Sort indices in ascending order
-
-    for (i, index) in to_remove.iter().enumerate() {
-        memory.remove(index - i); // Adjust for removed items before
-    }
-    return memory;
-}
-
-/// 関数の重複を削除する
-fn remove_duplicates_function(memory: &mut Vec<Func>) -> &mut Vec<Func> {
-    let mut seen_names = std::collections::HashMap::new();
-    let mut to_remove = Vec::new();
-
-    for (index, memory) in memory.iter().enumerate() {
-        if let Some(existing_index) = seen_names.get(&memory.name) {
-            to_remove.push(if existing_index < &index {
-                *existing_index
-            } else {
-                index
-            });
-        } else {
-            seen_names.insert(&memory.name, index);
-        }
-    }
-
-    to_remove.sort(); // Sort indices in ascending order
-
-    for (i, index) in to_remove.iter().enumerate() {
-        memory.remove(index - i); // Adjust for removed items before
-    }
-    memory
-}
-
 /// コードを実行を管理
 pub struct Executor {
     memory: Vec<Variable>, //メモリの変数
@@ -98,6 +49,7 @@ pub struct Executor {
 }
 
 impl Executor {
+    /// コンストラクタ
     pub fn new(memory: &Vec<Variable>, name_space: &Vec<Func>) -> Executor {
         Executor {
             memory: memory.to_owned(),
@@ -116,57 +68,68 @@ impl Executor {
         }
     }
 
+    /// 文の実行
     pub fn execute(&mut self, code: String) -> Option<f64> {
         let lines = code.trim_start().trim_end();
         if self.mode == "for".to_string() {
-            if lines.find("end for").is_some() {
+            if lines.contains("end for") {
+                // ネストの階層を判別する
                 if self.nest_for > 0 {
                     self.nest_for -= 1;
                     self.stmt += lines;
                     self.stmt += "\n";
                 } else {
                     for _ in 0..self.count {
-                        let status =  Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
+                        let status =
+                            Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
                         match status {
                             Some(i) => {
                                 if i == f64::MAX {
                                     //状態が1(break)の時はループを抜け出す
                                     break;
                                 } else {
+                                    // 戻り値を返す
                                     return Some(i);
                                 }
                             }
                             None => {}
                         }
-                    }
+                    } // モードを元に戻す
                     self.stmt = String::new();
                     self.mode = self.old_mode.clone();
                 }
             } else if lines.contains("for") {
+                // ネストの階層を上げる
                 self.nest_for += 1;
                 self.stmt += lines;
                 self.stmt += "\n";
             } else {
+                // コードを追加する
                 self.stmt += lines;
                 self.stmt += "\n";
             }
         } else if self.mode == "if".to_string() {
-            if lines.find("else").is_some() {
+            if lines.contains("else") {
+                // モードをelseに変える
                 self.old_mode = self.mode.clone();
                 self.mode = "else".to_string()
-            } else if lines.find("end if").is_some() {
+            } else if lines.contains("end if") {
                 if self.nest_if > 0 {
                     self.nest_if -= 1;
                     self.stmt += lines;
                     self.stmt += "\n";
                 } else {
-                    if self.calculation(self.expr.clone()) != 0.0{
-                        let status = Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
+                    // 条件式を評価する
+                    if self.calculation(self.expr.clone()) != 0.0 {
+                        let status =
+                            Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
                         match status {
                             Some(i) => {
                                 if i == f64::MAX {
+                                    // ループ階層へ渡す
                                     return Some(f64::MAX);
                                 } else {
+                                    // 戻り値を返す
                                     return Some(i);
                                 }
                             }
@@ -178,7 +141,7 @@ impl Executor {
                     }
                     self.mode = self.old_mode.clone();
                 }
-            } else if lines.find("if").is_some() {
+            } else if lines.contains("if") {
                 self.nest_if += 1;
                 self.stmt += lines;
                 self.stmt += "\n";
@@ -198,21 +161,22 @@ impl Executor {
                     self.stmt = String::new();
                     self.mode = self.old_mode.clone();
                 }
-            } else if lines.find("func").is_some() {
+            } else if lines.contains("func") {
                 self.nest_func += 1;
             } else {
                 self.stmt += lines;
                 self.stmt += &String::from("\n");
             }
         } else if self.mode == "else".to_string() {
-            if lines.find("end if").is_some() {
+            if lines.contains("end if") {
                 if self.nest_if > 0 {
                     self.nest_if -= 1;
                     self.stmt += lines;
                     self.stmt += "\n";
                 } else {
                     if self.calculation(self.expr.clone()) == 0.0 {
-                        let status =  Executor::new(&self.memory, &self.name_space).execute_block(&self.else_stmt);
+                        let status = Executor::new(&self.memory, &self.name_space)
+                            .execute_block(&self.else_stmt);
                         match status {
                             Some(i) => {
                                 if i == f64::MAX {
@@ -226,7 +190,8 @@ impl Executor {
                         self.else_stmt = String::new();
                         self.stmt = String::new();
                     } else {
-                        let status =  Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
+                        let status =
+                            Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
                         match status {
                             Some(i) => {
                                 if i == f64::MAX {
@@ -242,7 +207,7 @@ impl Executor {
                     }
                     self.mode = self.old_mode.clone();
                 }
-            } else if lines.find("if").is_some() {
+            } else if lines.contains("if") {
                 self.nest_if += 1;
                 self.stmt += lines;
                 self.stmt += "\n";
@@ -252,7 +217,7 @@ impl Executor {
                 self.else_stmt += &String::from("\n");
             }
         } else if self.mode == "while".to_string() {
-            if lines.find("end while").is_some() {
+            if lines.contains("end while") {
                 if self.nest_while > 0 {
                     self.nest_while -= 1;
                 } else {
@@ -261,7 +226,8 @@ impl Executor {
                             self.stmt = String::new();
                             break;
                         } else {
-                            let status =  Executor::new(&self.memory, &self.name_space).execute_block(&self.stmt);
+                            let status = Executor::new(&self.memory, &self.name_space)
+                                .execute_block(&self.stmt);
                             match status {
                                 Some(i) => {
                                     if i == f64::MAX {
@@ -277,14 +243,14 @@ impl Executor {
                     }
                     self.mode = self.old_mode.clone();
                 }
-            } else if lines.find("while").is_some() {
+            } else if lines.contains("while") {
                 self.nest_while += 1;
             } else {
                 self.stmt += lines;
                 self.stmt += "\n";
             }
         } else {
-            if lines.find("var").is_some() {
+            if lines.contains("var") {
                 let new_lines = lines.replacen("var", "", 1);
                 let lines = &new_lines;
                 let params: Vec<&str> = lines.split("=").collect();
@@ -294,7 +260,7 @@ impl Executor {
                     value,
                     expr: params[1..].join("=").to_string(),
                 });
-            } else if lines.find("calc").is_some() {
+            } else if lines.contains("calc") {
                 let new_lines = lines.replacen("calc", "", 1);
                 let name = &new_lines;
                 match self.reference_variable(name.to_string()) {
@@ -304,35 +270,36 @@ impl Executor {
                     }
                     None => {}
                 }
-            } else if lines.find("func").is_some() {
+            } else if lines.contains("func") {
                 let new_lines = lines.trim().replacen("func", "", 1).replace(" ", "");
                 self.name = new_lines.replace(" ", "");
                 self.mode = "func".to_string();
-            } else if lines.find("call").is_some() {
+            } else if lines.contains("call") {
                 let new_lines = lines.replacen("call", "", 1);
                 let name = &new_lines.replace(" ", "");
                 match self.reference_function(name.clone()) {
                     Some(index) => {
-                         Executor::new(&self.memory, &self.name_space).execute_block(&self.name_space[index].code);
+                        Executor::new(&self.memory, &self.name_space)
+                            .execute_block(&self.name_space[index].code);
                     }
                     None => {}
                 }
-            } else if lines.find("for").is_some() {
+            } else if lines.contains("for") {
                 let new_lines = lines.replacen("for", "", 1);
                 self.count = self.calculation(new_lines) as usize;
                 self.old_mode = self.mode.clone();
                 self.mode = "for".to_string();
-            } else if lines.find("if").is_some() {
+            } else if lines.contains("if") {
                 let new_lines = lines.replacen("if", "", 1);
                 self.expr = new_lines;
                 self.old_mode = self.mode.clone();
                 self.mode = "if".to_string()
-            } else if lines.find("while").is_some() {
+            } else if lines.contains("while") {
                 let new_lines = lines.replacen("while", "", 1);
                 self.expr = new_lines;
                 self.old_mode = self.mode.clone();
                 self.mode = "while".to_string();
-            } else if lines.find("input").is_some() {
+            } else if lines.contains("input") {
                 let new_lines = lines.replacen("input", "", 1);
                 let name = &new_lines;
                 let inputed = input("> ");
@@ -342,12 +309,12 @@ impl Executor {
                     value: value,
                     expr: inputed,
                 });
-            } else if lines.find("print").is_some() {
+            } else if lines.contains("print") {
                 let new_lines = lines.replacen("print", "", 1);
                 let mut text = String::new();
                 let params = &new_lines;
                 for i in params.split(",").collect::<Vec<&str>>() {
-                    if i.find("'").is_some() || i.find("\"").is_some() {
+                    if i.contains("'") || i.contains("\"") {
                         //文字列か？
                         text += &i.replace("'", "").replace("\"", "");
                     } else {
@@ -356,7 +323,7 @@ impl Executor {
                     }
                 }
                 println!("{text}");
-            } else if lines.find("del").is_some() {
+            } else if lines.contains("del") {
                 let new_lines = lines.replacen("del", "", 1);
                 let name = &new_lines;
                 match self.reference_variable(name.clone()) {
@@ -371,8 +338,8 @@ impl Executor {
                     }
                     None => {}
                 }
-            } else if lines.find("#").is_some() {
-            } else if lines.find("rand").is_some() {
+            } else if lines.contains("#") {
+            } else if lines.contains("rand") {
                 let new_lines = lines.replacen("rand", "", 1);
                 let params = new_lines.split(",").collect::<Vec<&str>>();
                 if params.len() < 3 {
@@ -395,10 +362,10 @@ impl Executor {
                         expr: temp.to_string(),
                     });
                 }
-            } else if lines.find("return").is_some() {
+            } else if lines.contains("return") {
                 let return_value = lines.replacen("return", "", 1);
                 return Some(self.calculation(return_value));
-            } else if lines.find("break").is_some() {
+            } else if lines.contains("break") {
                 return Some(f64::MAX);
             } else if lines == "exit" {
                 exit(0);
@@ -406,15 +373,17 @@ impl Executor {
             } else {
                 println!("コマンドが不正です: {}", lines)
             }
-            remove_duplicates_variable(&mut self.memory);
-            remove_duplicates_function(&mut self.name_space);
+            self.remove_duplicates_variable();
+            self.remove_duplicates_function();
         }
         return None;
     }
 
+    /// ブロックを実行
     pub fn execute_block(&mut self, code: &String) -> Option<f64> {
         for lin in code.split("\n") {
             match self.execute(lin.to_string()) {
+                // 戻り値を返す
                 Some(i) => return Some(i),
                 None => {}
             }
@@ -422,6 +391,7 @@ impl Executor {
         return None;
     }
 
+    /// 変数の参照
     fn reference_variable(&self, name: String) -> Option<usize> {
         let name = name.trim().replace(" ", "");
         match self
@@ -437,7 +407,7 @@ impl Executor {
         }
     }
 
-    ///　関数の参照
+    /// 関数の参照
     fn reference_function(&self, name: String) -> Option<usize> {
         let name = name.trim().replace(" ", "");
         match self
@@ -479,6 +449,55 @@ impl Executor {
         }
     }
 
+    /// 変数の重複を削除する
+    fn remove_duplicates_variable(&mut self) {
+        let mut seen_names = std::collections::HashMap::new();
+        let mut to_remove = Vec::new();
+
+        for (index, memory) in self.memory.iter().enumerate() {
+            if let Some(existing_index) = seen_names.get(&memory.name) {
+                to_remove.push(if existing_index < &index {
+                    *existing_index
+                } else {
+                    index
+                });
+            } else {
+                seen_names.insert(&memory.name, index);
+            }
+        }
+
+        to_remove.sort(); // Sort indices in ascending order
+
+        for (i, index) in to_remove.iter().enumerate() {
+            self.memory.remove(index - i); // Adjust for removed items before
+        }
+    }
+
+    /// 関数の重複を削除する
+    fn remove_duplicates_function(&mut self) {
+        let mut seen_names = std::collections::HashMap::new();
+        let mut to_remove = Vec::new();
+
+        for (index, memory) in self.memory.iter().enumerate() {
+            if let Some(existing_index) = seen_names.get(&memory.name) {
+                to_remove.push(if existing_index < &index {
+                    *existing_index
+                } else {
+                    index
+                });
+            } else {
+                seen_names.insert(&memory.name, index);
+            }
+        }
+
+        to_remove.sort(); // Sort indices in ascending order
+
+        for (i, index) in to_remove.iter().enumerate() {
+            self.memory.remove(index - i); // Adjust for removed items before
+        }
+    }
+
+    /// 式の計算
     fn calculation(&mut self, expr: String) -> f64 {
         let mut stack: Vec<f64> = Vec::new();
         let tokens = expr.split_whitespace();
@@ -523,7 +542,9 @@ impl Executor {
                             }
                             match self.reference_function_quiet(i.to_string()) {
                                 Some(i) => stack.push(
-                                    match  Executor::new(&self.memory, &self.name_space).execute_block(&self.name_space[i].code) {
+                                    match Executor::new(&self.memory, &self.name_space)
+                                        .execute_block(&self.name_space[i].code)
+                                    {
                                         Some(i) => i,
                                         None => 0.0,
                                     },
@@ -586,7 +607,9 @@ impl Executor {
                             match self.reference_function_quiet(i.to_string()) {
                                 Some(index) => stack.push({
                                     println!("関数{i}を呼び出します");
-                                    match  Executor::new(&self.memory, &self.name_space).execute_block(&self.name_space[index].code) {
+                                    match Executor::new(&self.memory, &self.name_space)
+                                        .execute_block(&self.name_space[index].code)
+                                    {
                                         Some(indes) => indes,
                                         None => 0.0,
                                     }
