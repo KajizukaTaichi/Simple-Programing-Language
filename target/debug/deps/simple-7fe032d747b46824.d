d71e1618bd56d57ab8c23ca8002e8660687665ff
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-12-19 08:40:28 +0900

    Delete target directory

--- target/debug/deps/simple-7fe032d747b46824.d
+++
@@ -1,8 +0,0 @@
-C:\Users\admin\OneDrive\デスクトップ\プログラミング\リポジトリ\Simple-Programing-Language\target\debug\deps\libsimple-7fe032d747b46824.rmeta: src\main.rs src\checker.rs src\executor.rs src\stdlib.rs
-
-C:\Users\admin\OneDrive\デスクトップ\プログラミング\リポジトリ\Simple-Programing-Language\target\debug\deps\simple-7fe032d747b46824.d: src\main.rs src\checker.rs src\executor.rs src\stdlib.rs
-
-src\main.rs:
-src\checker.rs:
-src\executor.rs:
-src\stdlib.rs:
--- target/release/deps/simple.d
+++
@@ -1,8 +0,0 @@
-C:\Users\admin\OneDrive\デスクトップ\プログラミング\リポジトリ\Simple-Programing-Language\target\release\deps\simple.exe: src\main.rs src\checker.rs src\executor.rs src\stdlib.rs
-
-C:\Users\admin\OneDrive\デスクトップ\プログラミング\リポジトリ\Simple-Programing-Language\target\release\deps\simple.d: src\main.rs src\checker.rs src\executor.rs src\stdlib.rs
-
-src\main.rs:
-src\checker.rs:
-src\executor.rs:
-src\stdlib.rs:
